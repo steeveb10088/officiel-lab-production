ce4a8b76148126ec88d8576f9e4fe2368ffd5839
richard steeve blanchard <steeveb1008@gmail.com>
2024-12-10 11:16:44 -0500

    Add config port

--- src/main/java/ca/ulaval/glo4002/cart/CartServer.java
+++ src/main/java/ca/ulaval/glo4002/cart/CartServer.java
@@ -20,7 +20,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class CartServer implements Runnable {
-    private static final int PORT = 7222;
+    private static final int PORT =  Integer.parseInt(System.getenv("PORT"));
 
     public static void main(String[] args) {
         new CartServer().run();
